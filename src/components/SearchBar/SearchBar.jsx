b58112edf78f54213a3dcd89d5d87b742c7b5550
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-08-07 20:56:59 +1000

    search bar fetching api

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,12 +2,30 @@ import { useState } from "react"
 
 
 export default function SearchBar() {
-
   const [searchInput, setSearchInput] = useState('');
   const [Autocomplete, setAutocomplete] = useState([]);
 
-  const handleChange = (evt) => {
+  const fetchFoodAutocomplete = async (query) => {
+    const apiKey = 'wgQutoUF16Z9JEHWDmdCkY4CJol4FwrwzdJUjIe4';
+    try {
+      const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=${query}`)
+      const data = await response.json();
+      console.log(data);
+      setAutocomplete(data.foods);
+    } catch (error) {
+      console.error('Error fetching food suggestions', error);
+    }
+    }
 
+
+  const handleChange = (evt) => {
+    const query = evt.target.value;
+    setSearchInput(query);
+    if (query.length>3){
+      fetchFoodAutocomplete(query);
+    } else {
+      setAutocomplete([])
+    }
 
   }
 
@@ -18,5 +36,10 @@ export default function SearchBar() {
     onChange={handleChange}
     value={searchInput}
     />
+    <ul>
+      {Autocomplete.map((foodSuggestions) => (
+        <li key={foodSuggestions.fdcId}>{foodSuggestions.description}</li>
+      ))}
+    </ul>
     </div>
 }
