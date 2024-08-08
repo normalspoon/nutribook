fa0d072931aa30b6f370deb1187afb61a85990a2
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-08-08 15:54:14 +1000

    adding more schema

--- models/dailyMealPlan.js
+++ models/dailyMealPlan.js
@@ -3,10 +3,29 @@ const Schema = mongoose.Schema;
 // const bcrypt = require('bcrypt');
 
 const mealSchema = new Schema({
-    description: {type: Stringm, required: true},
-
-    
-
+    description: {type: String, required: true},
+    energy: { type: Number },
+    protein: { type: Number},
+    carbs: { type: Number},
+    fat: {type: Number},
+    sugars: {type: Number},
+    fiber: {type: Number},
+    iron: {type: Number},
+    sodium: {type: Number},
+    magnesium: {type: Number},
+    nicacin: {type: Number},
+    vitaminA: {type: Number},
+    vitaminD: {type: Number},
+    vitaminC: {type: Number},
+    vitaminK: {type: Number},
+    vitaminB12: {type: Number},
+    vitamin: {type: Number},
+    lycopene: {type: Number},
+    luteinZeazanthin: {type: Number},
+    cholesterol: {type: Number},
+    saturatedFat: {type: Number},
+    Omega3EPA: {type: Number},
+    Omega3DHA: {type: Number},
 })
 
 
