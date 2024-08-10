1573a94fc9fa7419eed89812297db0ebb5173c9c
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-08-10 15:08:20 +1000

    fixing math

--- src/pages/DailyMealPage/DailyMealPage.jsx
+++ src/pages/DailyMealPage/DailyMealPage.jsx
@@ -48,25 +48,25 @@ export default function DailyMealPage() {
         <div className='dailyMeals'>
                 <h2>Breakfast</h2>
                 {mealPlan.breakfast.map((meal, index) => (
-                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy %100) * meal.amount)} kcal</div>
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
                 ))}
                 <AddMealButton mealType="breakfast" onMealAdd={handleMealAdd}/>
                 
                 <h2>Lunch</h2>
                 {mealPlan.lunch.map((meal, index) => (
-                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy %100) * meal.amount)} kcal</div>
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
                 ))}
                 <AddMealButton mealType="lunch" onMealAdd={handleMealAdd}/>
                 
                 <h2>Dinner</h2>
                 {mealPlan.dinner.map((meal, index) => (
-                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy %100) * meal.amount)} kcal</div>
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
                 ))}
                 <AddMealButton mealType="dinner" onMealAdd={handleMealAdd}/>
                 
                 <h2>Snack</h2>
                 {mealPlan.snack.map((meal, index) => (
-                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy %100) * meal.amount)} kcal</div>
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
                 ))}
                 <AddMealButton mealType="snack" onMealAdd={handleMealAdd}/>
             </div>
