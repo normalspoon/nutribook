9d57dda00e9f0d0b2ce7f561d6e55a967476cf88
Daniel Poon <danielpoon@Daniels-MacBook-Pro-2.local>
2024-08-13 11:04:39 +1000

    made edit page layout

--- src/pages/EditMealPage/EditMealPage.jsx
+++ src/pages/EditMealPage/EditMealPage.jsx
@@ -3,71 +3,112 @@ import { useParams } from "react-router-dom";
 import { PlansContext } from "../../context/PlansContext";
 import { Link } from "react-router-dom";
 import './EditMealPage.css';
+import  SearchBar  from "../../components/SearchBar/SearchBar"
+import AddMealButton from "../../components/AddMealButton/AddMealButton";
+import { createPlan } from "../../utilities/plans-api";
 
+    
 export default function EditMealPage() {
-  const { id } = useParams();
-  const { plans } = useContext(PlansContext);
-  const [selectedPlan, setSelectedPlan] = useState(null);
-  const [totalNutritionScore, setTotalNutritionScore] = useState({
-    energy: 0,
-    protein: 0,
-    carbs: 0,
-    fat: 0,
-    sugars: 0,
-  });
+    const [mealPlan, setMealPlan] = useState({
+        name: '',
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+        snack: [],
+    });
 
-  const fetchPlanDetails = async () => {
-    const planDetails = plans.find((plan) => plan._id === id);
-    console.log('planDetails', planDetails);
-    setSelectedPlan(planDetails);
-    calculateTotalNutritionScore(planDetails);
-  };
+    const { id } = useParams();
+    const { plans } = useContext(PlansContext);
+    const [selectedPlan, setSelectedPlan] = useState(null);
+    const [totalNutritionScore, setTotalNutritionScore] = useState({
+      energy: 0,
+      protein: 0,
+      carbs: 0,
+      fat: 0,
+      sugars: 0,
+    });
+    const fetchPlanDetails = async () => {
+        const planDetails = plans.find((plan) => plan._id === id);
+        console.log('planDetails', planDetails);
+        setSelectedPlan(planDetails);
+        calculateTotalNutritionScore(planDetails);
+      }; 
+    
+    function calculateTotalNutritionScore(planDetails) {
+        const meals = ['breakfast', 'lunch', 'dinner', 'snack'];
+        const totals = { energy: 0, protein: 0, carbs: 0, fat: 0, sugars: 0 };
+    
+        meals.forEach(function(meal) {
+            console.log('meal', meal);
+            planDetails[meal].forEach(function(food) {
+              console.log('planDetails', planDetails, 'food', food);
+              totals.energy += food.energy;
+              totals.protein += food.protein;
+              totals.carbs += food.carbs;
+              totals.fat += food.fat;
+              totals.sugars += food.sugars;
+            });
+          });
+          console.log('totals', totals);
+          setTotalNutritionScore(totals);
+      }
+    
+    
+      useEffect(() => {
+        fetchPlanDetails();
+      }, [id, plans]);
+    
+      console.log('selectedPlan', selectedPlan);
+      if (!selectedPlan) {
+        return <div>Loading...</div>;
+      }
 
-function calculateTotalNutritionScore(planDetails) {
-    const meals = ['breakfast', 'lunch', 'dinner', 'snack'];
-    const totals = { energy: 0, protein: 0, carbs: 0, fat: 0, sugars: 0 };
 
-    meals.forEach(function(meal) {
-        console.log('meal', meal);
-        planDetails[meal].forEach(function(food) {
-          console.log('planDetails', planDetails, 'food', food);
-          totals.energy += food.energy;
-          totals.protein += food.protein;
-          totals.carbs += food.carbs;
-          totals.fat += food.fat;
-          totals.sugars += food.sugars;
+
+
+
+
+
+    function handleSaveMealPlan(){
+        createPlan(mealPlan).then(response => {
+            console.log('Meal plann saved', response);
+        }).catch(error => {
+            console.error('Error saving meal plan', error);
         });
-      });
-      console.log('totals', totals);
-      setTotalNutritionScore(totals);
-  }
+    }
+
+    function handleMealAdd(mealType, meal){
+        setMealPlan(prevState => ({
+            ...prevState,
+            [mealType]: [...prevState[mealType], meal]
+        }))
+    }
+
+    function handleNameChange(event) {
+        setMealPlan(prevState => ({
+            ...prevState,
+            name: event.target.value,
+        }));
+    }
 
 
-  useEffect(() => {
-    fetchPlanDetails();
-  }, [id, plans]);
-
-  console.log('selectedPlan', selectedPlan);
-  if (!selectedPlan) {
-    return <div>Loading...</div>;
-  }
-
-  function handleEdit() {
-    console.log('Edit button clicked');
-
-  }
-
-  return (
-    <>
-      <h1>Meal Plan Details</h1>
-      <div className="mealHeader">
-        <h1>{selectedPlan.name}</h1> 
-        <Link to={`/edit/${id}`}>
-        <button className="editButton" onClick={handleEdit}>Edit</button>
-        </Link>
-      </div>
-      <h2>Breakfast</h2>
-      {selectedPlan.breakfast.map((food, index) => (
+    return (
+        <div>
+        <h1>Edit Meal Page</h1>
+        <div>
+            <h2>Name:</h2>
+            <input type="text" 
+            placeholder={`${selectedPlan.name}`}
+            value={mealPlan.name}
+            onChange={handleNameChange}/>
+        </div>
+        <div className='dailyMeals'>
+                <h2>Breakfast</h2>
+                <AddMealButton mealType="breakfast" onMealAdd={handleMealAdd}/>
+                {mealPlan.breakfast.map((meal, index) => (
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
+                ))}
+                {selectedPlan.breakfast.map((food, index) => (
         <div key={index}>
           <p>{food.name}</p>
           <ul>
@@ -75,8 +116,14 @@ function calculateTotalNutritionScore(planDetails) {
           </ul>
         </div>
       ))}
-      <h2>Lunch</h2>
-      {selectedPlan.lunch.map((food, index) => (
+                
+                
+                <h2>Lunch</h2>
+                <AddMealButton mealType="lunch" onMealAdd={handleMealAdd}/>
+                {mealPlan.lunch.map((meal, index) => (
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
+                ))}
+                   {selectedPlan.lunch.map((food, index) => (
         <div key={index}>
           <p>{food.name}</p>
           <ul>
@@ -84,8 +131,14 @@ function calculateTotalNutritionScore(planDetails) {
           </ul>
         </div>
       ))}
-      <h2>Dinner</h2>
-      {selectedPlan.dinner.map((food, index) => (
+                
+                
+                <h2>Dinner</h2>
+                <AddMealButton mealType="dinner" onMealAdd={handleMealAdd}/>
+                {mealPlan.dinner.map((meal, index) => (
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
+                ))}
+                   {selectedPlan.dinner.map((food, index) => (
         <div key={index}>
           <p>{food.name}</p>
           <ul>
@@ -93,8 +146,14 @@ function calculateTotalNutritionScore(planDetails) {
           </ul>
         </div>
       ))}
-      <h2>Snacks</h2>
-      {selectedPlan.snack.map((food, index) => (
+                
+                
+                <h2>Snack</h2>
+                <AddMealButton mealType="snack" onMealAdd={handleMealAdd}/>
+                {mealPlan.snack.map((meal, index) => (
+                    <div key={index}>{meal.name} - {meal.amount} grams - {((meal.energy /100) * meal.amount)} kcal</div>
+                ))}
+                {selectedPlan.snack.map((food, index) => (
         <div key={index}>
           <p>{food.name}</p>
           <ul>
@@ -102,7 +161,13 @@ function calculateTotalNutritionScore(planDetails) {
           </ul>
         </div>
       ))}
-      <div className="nutritionCardContainer">
+            </div>
+            <div className='nutritionScore'>
+            <Link to= "/">
+                <button onClick={handleSaveMealPlan}>Save Meal Plan</button>
+            </Link>
+    
+                <div className="nutritionCardContainer">
         <h2>Total Nutrition Score</h2>
         <table>
           <thead>
@@ -134,6 +199,13 @@ function calculateTotalNutritionScore(planDetails) {
           </thead>
         </table>
       </div>
-    </>
-  );
+            </div>
+        </div>
+    );
 }
+
+
+
+
+
+
